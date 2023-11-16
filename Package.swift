cb9eace3a76faf21c47fe285a5046c061244848a
DenisOvchat <fitil457@gmail.com>
2023-11-16 08:07:36 +0530

    ver

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
 
-        .package(url: "https://github.com/HyperfocusDisordered/FoundationExtensions.git", .exact("2.0.0")),
+        .package(url: "https://github.com/HyperfocusDisordered/FoundationExtensions.git", .exact("2.0.1")),
     ],
     targets: [
 			.target(name: "CombineOperators", dependencies: ["FoundationExtensions"]),
