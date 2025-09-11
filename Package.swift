fa8b2669971672a73bed3e397ad9fbc119a305ee
Denis <denisovchar@Deniss-MacBook-Pro.local>
2025-09-12 03:01:04 +0400

    2.11.0

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
 
-        .package(url: "https://github.com/HyperfocusDisordered/FoundationExtensions.git", .exact("2.10.0")),
+        .package(url: "https://github.com/HyperfocusDisordered/FoundationExtensions.git", .exact("2.11.0")),
     ],
     targets: [
 			.target(name: "CombineOperators", dependencies: ["FoundationExtensions"]),
